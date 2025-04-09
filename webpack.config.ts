93cdcf4bae2b474484f7fadc1979372072b19419
Tenadopt <yury.ignatiev@gmail.com>
2025-04-09 12:03:26 +0200

    feat: add ts, webpack, eslint, folders structure

---
+++ webpack.config.ts
@@ -0,0 +1,31 @@
+import path from 'path';
+import { Configuration } from 'webpack';
+import 'webpack-dev-server';
+
+const config: Configuration = {
+  entry: './src/index.ts',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  devServer: {
+    static: './dist',
+    port: 8080,
+    open: true,
+  },
+  mode: 'development',
+};
+
+export default config;
